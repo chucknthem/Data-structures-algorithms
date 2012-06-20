cfea8878b6b450b628f91176fed5cce212c8e0c2
Charles Ma <chuckjma@gmail.com>
2012-06-19 21:04:36 -0700

    fixes and tests for linked_list.c

--- structs/linked_list.c
+++ structs/linked_list.c
@@ -2,41 +2,58 @@
 #include <stdlib.h>
 #include <malloc/malloc.h>
 
+#define INVALID_VALUE -1
+
 typedef struct NodeT {
-  struct NodeT *next;
+  struct NodeT* next;
   int value;
 } Node;
 
 typedef struct ListT {
-  Node *head;
+  Node* head;
   int length;
 } List;
 
-List *createList() {
-  List *newList = (List*) malloc(sizeof(List));
+List* createList() {
+  List* newList = (List*) malloc(sizeof(List));
   newList->head = NULL;
   newList->length = 0;
   return newList;
 }
 
-Node *createNode(int value) {
-  Node *newNode = (Node*) malloc(sizeof(Node));
+Node* createNode(int value) {
+  Node* newNode = (Node*) malloc(sizeof(Node));
   newNode->next = NULL;
   newNode->value = value;
   return newNode;
 }
 
+void destroyNode(Node* node) {
+  free(node);
+}
+
+void destroyList(List* list) {
+  if (!list) return;
+  while (list->head) {
+    Node* next = list->head->next;
+    free(list->head);
+    list->head = next;
+  }
+
+  free(list);
+}
+
 /**
  * Insert value at position. The list is zero indexed.
  */
-void insertNode(List *ls, int value, int position) {
-  Node *newNode = createNode(value);
+void insertNode(List* ls, int value, int position) {
+  Node* newNode = createNode(value);
   if (position == 0 || ls->head == NULL) {
     newNode->next = ls->head;
     ls->head = newNode;
   }
-  Node *cur = ls->head;
-  Node *prev = NULL;
+  Node* cur = ls->head;
+  Node* prev = NULL;
   while(position-- && cur) 
     prev = cur;
     cur = cur->next;
@@ -49,11 +66,11 @@ void insertNode(List *ls, int value, int position) {
 /**
  * Delete the node at position where position starts at 0.
  */
-void removeNode(List *ls, int position) {
+void removeNode(List* ls, int position) {
   if (position >= ls->length) return;
 
-  Node *cur = ls->head;
-  Node *prev = NULL;
+  Node* cur = ls->head;
+  Node* prev = NULL;
 
   if (position == 0) {
     ls->head = ls->head->next;
@@ -68,19 +85,116 @@ void removeNode(List *ls, int position) {
   ls->length--;
 }
 
-void push(List *ls, int value) {
-  Node *newNode = createNode(value);
+void push(List* ls, int value) {
+  Node* newNode = createNode(value);
   newNode->next = ls->head;
   ls->head = newNode;
   ls->length++;
 }
 
-int pop(List *ls) {
-  Node *toDelete = ls->head;
-  int toReturn = toDelete->value;
-  ls->head = ls->head->next;
-  free(toDelete);
-  ls->length--;
-  return toReturn;
+void pushNode(List* ls, Node* node) {
+  node->next = ls->head;
+  ls->head = node;
+  ls->length++;
+}
+
+int pop(List* ls) {
+  Node* node = ls->head; 
+  int value = INVALID_VALUE;
+  if (node) {
+    ls->head = node->next;
+    ls->length--;
+    value = node->value;
+    destroyNode(node);
+  }
+
+  return value;
+}
+
+Node* popNode(List* ls) {
+  Node* node = ls->head;
+  if (node) {
+    ls->head = node->next;
+    ls->length--;
+  }
+
+  return node;
+}
+
+/* 
+ * What is the reverse of null (the empty list)? null.
+ * What is the reverse of a one element list? the element.
+ * What is the reverse of an n element list? the reverse of the second element on followed by the first element.
+ */
+Node* reverseRecursiveNodeList(Node* node) {
+  if (!(node && node->next)) return node;
+  Node* next = node->next;
+  node->next = NULL;
+  Node* rest = reverseRecursiveNodeList(next);
+  next->next = node;
+  return rest;
+}
+
+Node* reverseNodeList(Node* node) {
+  if (!(node && node->next)) 
+    return node;
+  Node* cur = node;
+  Node* next = node->next;
+  node->next = NULL;
+  Node* tmp;
+  while (next) {
+    tmp = next->next;
+    next->next = cur;
+    cur = next;
+    next = tmp;
+  }
+
+  return cur;
+}
+
+void reverseRecursive(List* ls) {
+  ls->head = reverseRecursiveNodeList(ls->head);
+}
+
+void reverse(List* ls) {
+  ls->head = reverseNodeList(ls->head);
+}
+
+void printList(List* list) {
+  Node* current;
+  for (current = list->head; current != NULL; current = current->next) {
+    printf("%d ", current->value);
+  }
+  putchar('\n');
+}
+
+int main(void) {
+  List* list = createList();
+  push(list, 4);
+  push(list, 8);
+  push(list, 1);
+  push(list, 9);
+  push(list, 3);
+  push(list, 5);
+  push(list, 8);
+  push(list, 7);
+
+  printf("starting list\n");
+  printList(list);
+
+  int top = pop(list);
+  printf("popped %d\n", top);
+  printList(list);
+
+  reverse(list);
+  printf("iterative reverse\n");
+  printList(list);
+
+  reverseRecursive(list);
+  printf("recursive reverse\n");
+  printList(list);
+  
+  destroyList(list);
+  return 0;
 }
 
